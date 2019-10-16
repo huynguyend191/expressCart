12001db84cf329cf0356ba9301d8b06e4a8edfc1
Huy Nguyen <huynguyend191@gmail.com>
2019-10-16 13:56:32 +0700

    destroy session when payment complete

--- routes/index.js
+++ routes/index.js
@@ -20,12 +20,14 @@ const {
 
 router.get('/paymentFake', (req, res, next) => {
   const customerEmail = req.session.customer.email;
+  const address = req.session.customer.address1;
   const total = req.session.totalCartAmount;
   let productList = '';
   req.session.cart.forEach(product => {
     productList += `${product.productId} ${product.quantity}, `;
   });
-  log.printLog('PAYMENT', req.sessionID, `"${customerEmail}" "${total}" "${productList.substring(0, productList.length - 2)}"`);
+  log.printLog('PAYMENT', req.sessionID, `"${customerEmail}" "${address}" "${total}" "${productList.substring(0, productList.length - 2)}"`);
+  req.session.destroy();
   res.status(200).json({ message: 'Payment successful' });
 })
 // These is the customer facing routes
